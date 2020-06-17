62c415b2cf594064cefd3f4983ba8e05e4eda99a
m431m <mmeliani@neogeo.fr>
2020-06-17 11:56:18 +0200

    1.0.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from distutils.core import setup
 
 setup(
     name='MapServer Rest API',
-    version='1.0.1',
+    version='1.0.2',
     author='Neogeo Technologies',
     author_email='contact@neogeo-online.net',
     description='A RESTFul interface for MapServer',
